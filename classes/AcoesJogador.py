560e5e6d1681afb3347468fd3b4fb5b7216914c3
cadu1010 <109558659+cadu1010@users.noreply.github.com>
2022-07-25 20:11:46 -0300

    Update AcoesJogador.py
    trocamos o "0" pelo "auto()"

--- classes/AcoesJogador.py
+++ classes/AcoesJogador.py
@@ -1,7 +1,7 @@
 import enum
 class AcoesJogador(enum):
-    selecao = 0 #seleção de personagem
-    escolha_carta = 0 #pegar duas cartas e escolher uma
-    escolha_moedas = 0
-    construcao = 0 
-    habilidade = 0
+    selecao = auto() #seleção de personagem
+    escolha_carta = auto() #pegar duas cartas e escolher uma
+    escolha_moedas = auto()
+    construcao = auto()
+    habilidade = auto()
