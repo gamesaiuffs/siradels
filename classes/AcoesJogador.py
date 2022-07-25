cc395fa243b937e6fe046614b87685bdb16f2c4a
cadu1010 <109558659+cadu1010@users.noreply.github.com>
2022-07-25 20:05:07 -0300

    AcoesJogador
    adicinou-se a classe de ações do Jogador (turno) > AcoesJogador

---
+++ classes/AcoesJogador.py
@@ -0,0 +1,7 @@
+import enum
+class AcoesJogador(enum):
+    selecao = 0 #seleção de personagem
+    escolha_carta = 0 #pegar duas cartas e escolher uma
+    escolha_moedas = 0
+    construcao = 0 
+    habilidade = 0
