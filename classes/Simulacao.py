8f8ce4379d8763277a127d7749ca5fd3d4978d7c
grandofelipe <100877161+grandofelipe@users.noreply.github.com>
2022-07-25 21:33:25 -0300

    Correções na classe simulacao

--- classes/Simulacao.py
+++ classes/Simulacao.py
@@ -4,9 +4,9 @@ import Tabuleiro
 
 class Simulacao:
     def __init__(self, num_jogadores: int, automatico: bool):
-        self.estado = self.criar_estado_inicial() 
+        self.estado = self.criar_estado_inicial(automatico) 
 
-    def criar_estado_inicial() -> Estado:
+    def criar_estado_inicial(automatico) -> Estado:
         tabuleiro = Tabuleiro(num_jogadores)
         if automatico:
             jogadores = criar_jogadores_automatico()
@@ -23,9 +23,6 @@ class Simulacao:
     def criar_jogadores(num_jogadores:int) -> list(Jogador):
         #todo
         #criar jogadores iniciais
-
-    def criar_tabuleiro() -> Tabuleiro:
-        #todo
 
     def distribuir_cartas_iniciais()
 
