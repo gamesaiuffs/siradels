9488bf637484f78a20e32cbe113f2d734aeb975f
Eduardo Fiorentin <eduardoviniciusfiorentin@gmail.com>
2022-08-01 20:25:27 -0300

    Adição dos métodos criar jogadores

--- classes/Jogador.py
+++ classes/Jogador.py
@@ -4,16 +4,16 @@ import CartaPersonagem
 
 class Jogador:
     # variavel order removida 
-    def __init__(self, pontuacao: int, nome: str, carta_personagem: CartaPersonagem, ouro: int, cartas_distritos_na_mao: list(CartaDistrito), distritos_construidos: list(CartaDistrito), eh_o_rei: bool, morto: bool, roubado: bool):
-        self.pontuacao = pontuacao 
+    def __init__(self, nome: str):
+        self.pontuacao = 0 
         self.nome = nome 
-        self.carta_personagem = carta_personagem
-        self.ouro = ouro
-        self.cartas_distrito_na_mao = cartas_distritos_na_mao
-        self.distritos_construidos = distritos_construidos
-        self.eh_o_rei = eh_o_rei
-        self.morto = morto
-        self.roubado = roubado
+        self.carta_personagem = []
+        self.ouro = 2
+        self.cartas_distrito_na_mao = []
+        self.distritos_construidos = []
+        self.eh_o_rei = False
+        self.morto = False
+        self.roubado = False
 
     def __str__(self):
         return f"Pontuação:  {self.pontuacao} Nome:  {self.nome} Carta de personagem: {self.carta_personagem} Ouro:  {self.ouro} Cartas de distrito na mão:  {self.cartas_distrito_na_mao} É o rei:  {self.eh_o_rei}"
