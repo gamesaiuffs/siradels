40e750ca28362b36b3d8e63de7a328e57963f33e
Djonatan Bonelli <100157600+FenrirTG1@users.noreply.github.com>
2022-10-10 20:43:52 -0300

    Update Acao.py

--- classes/Acao.py
+++ classes/Acao.py
@@ -3,7 +3,6 @@ from abc import abstractmethod
 from Estado import Estado
 from Jogador import Jogador
 
-
 class Acao:
     # Construtor
     def __init__(self, descricao: str):
@@ -18,7 +17,6 @@ class Acao:
     def ativar_efeito(estado: Estado, jogador: Jogador) -> Estado:
         pass
 
-
 class ColetarOuro(Acao):
     def __init__(self):
         super().__init__('Colete 2 ouros do banco.')
@@ -27,28 +25,6 @@ class ColetarOuro(Acao):
     def ativar_efeito(estado: Estado, jogador_alvo: Jogador):
         estado.jogadores[estado.jogadores.index(jogador_alvo)].ouro += 2
 
-class ColetarCartas(Acao):
-    def __init__(self):
-        super().__init__('Colete 2 cartas do baralho e escolha uma.')
-
-    @staticmethod
-    def ativar_efeito(estado: Estado, jogador_alvo: Jogador):
-        escolherCartas = estado.tabuleiro.baralho_distritos[0:2]
-        estado.tabuleiro.baralho_distritos.pop()
-        estado.tabuleiro.baralho_distritos.pop()
-
-        print(escolherCartas[0])
-        print(escolherCartas[1])
-
-        escolha = int(input("Digite 1 para a primeira opção, 2 para a segunda: "))
-        if escolha == 1:
-            estado.jogadores[estado.jogadores.index(jogador_alvo)].cartas_distrito_mao.append(escolherCartas[0])
-            estado.tabuleiro.baralho_distritos.append(escolherCartas[0])
-        else:
-            estado.jogadores[estado.jogadores.index(jogador_alvo)].cartas_distrito_mao.append(escolherCartas[1])
-            estado.tabuleiro.baralho_distritos.append(escolherCartas[0])
-
-
 class EfeitoAssassino(Acao):
     def __init__(self):
         super().__init__('Anuncie um personagem que você deseja assassinar. O personagem assassinado perde o turno.')
@@ -56,3 +32,35 @@ class EfeitoAssassino(Acao):
     @staticmethod
     def ativar_efeito(estado: Estado, jogador_alvo: Jogador):
         estado.jogadores[estado.jogadores.index(jogador_alvo)].morto = True
+
+class EfeitoLadrao(Acao):
+    def __init__(self):
+        super().__init__('Anuncie um personagem que você deseja roubar. O personagem roubado entrega todo seu ouro ao ladrão.')
+
+    @staticmethod
+    def ativar_efeito(estado: Estado, jogador_alvo: Jogador):
+        estado.jogadores[estado.jogadores.index(jogador_alvo)].roubado = True
+
+class EfeitoMago(Acao):
+    def __init__(self):
+        super().__init__('Escolha um jogador para ver sua mão de distritos, em seguida, escolha uma carta e pague para construí-la imediaamente ou adiciona-a à sua mão. (Você pode construir distritos idênticos)')
+
+    @staticmethod
+    def ativar_efeito(estado: Estado, jogador_alvo: Jogador):
+        for numero_jogadores in range(len(estado.jogadores)):
+            print(estado.jogadores[numero_jogadores])
+
+        jogador_escolhido = input()
+
+        for cartas_disponiveis_mao in range(len(estado.jogadores[jogador_escolhido-1].cartas_distrito_mao)):
+            print(estado.jogadores[jogador_escolhido-1].cartas_distrito_mao[cartas_disponiveis_mao])
+
+        carta_escohida = input('Carta escolhida: ')
+        estado.jogadores[estado.jogadores.index(jogador_alvo)].cartas_distrito_mao.append(estado.jogadores[jogador_escolhido-1].cartas_distrito_mao[carta_escohida])
+
+class EfeitoRei(Acao):
+    def __init__(self):
+        super().__init__('Pegue a coroa. (Ganhe 1 ouro para cada um dos seus distritos NOBRES)')
+
+    @staticmethod
+    def ativar_efeito()
