4475d1025417847192ea4ea1b7d1b21d1bed4eac
Breno Soares <brenosoares4303@gmail.com>
2022-10-10 21:41:53 -0300

    finalizado a escolha de personagem por jogador

--- classes/Principal.py
+++ classes/Principal.py
@@ -9,20 +9,27 @@ final_jogo = False
 # Main Loop
 while not final_jogo:
     # Cada jogador escolhe seu personagem
+    foi_escolhido = []
     for jogador in simulacao.estado.jogadores:
         print("---------| Personagens |---------")
         # "Printa" o baralho de personagens
-        for personagem in simulacao.estado.tabuleiro.baralho_personagens:
-            print(personagem)
 
-        print("Escolha uma carta de personagem [Rank]: ", end="")
-        rank_carta_escolhida = int(input())
-        
-        print("=============================================================================")
-        for index, carta_personagem in enumerate(simulacao.estado.tabuleiro.baralho_personagens):
-            if carta_personagem.rank == rank_carta_escolhida:
-                jogador.personagem.append(simulacao.estado.tabuleiro.baralho_personagens.pop(index))
-                
+        while len(jogador.personagem) == 0:
+            print(jogador.nome)
+            for personagem in simulacao.estado.tabuleiro.baralho_personagens:
+                print(personagem)
+            print("Escolha uma carta de personagem [Rank]: ", end="")
+            rank_carta_escolhida = int(input())
+            
+            for index, carta_personagem in enumerate(simulacao.estado.tabuleiro.baralho_personagens):
+                if carta_personagem.rank == rank_carta_escolhida:
+                    jogador.personagem.append(simulacao.estado.tabuleiro.baralho_personagens.pop(index))
+                    foi_escolhido.append(rank_carta_escolhida)
+                    break
+                if rank_carta_escolhida in foi_escolhido:
+                    print("Escolha inválida")
+                    break
+            
     # Ordena os jogadores
     simulacao.estado.ordenar_jogadores()
     # Cada jogador faz suas ações
