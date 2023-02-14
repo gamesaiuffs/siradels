aa152958927c51ced8b8faba176a64fef0cd541b
grandofelipe <felipegrando@uffs.edu.br>
2023-02-14 15:20:48 -0300

    Ajustes na descrição das cartas

--- classes/CartaDistrito.py
+++ classes/CartaDistrito.py
@@ -4,16 +4,20 @@ from TipoDistrito import TipoDistrito
 
 class CartaDistrito:
     # Construtor
-    def __init__(self, valor_do_distrito: int, tipo_de_distrito: TipoDistrito, nome_do_distrito: str, quantidade: int):
+    def __init__(self, valor_do_distrito: int, tipo_de_distrito: TipoDistrito, nome_do_distrito: str, quantidade: int, efeito: str = ""):
         self.valor_do_distrito = valor_do_distrito
         self.tipo_de_distrito = tipo_de_distrito
         self.nome_do_distrito = nome_do_distrito
         self.quantidade = quantidade
+        self.efeito = efeito
 
     # To String
     def __str__(self) -> str:
         return f'{self.nome_do_distrito}'
     
     def imprimir_tudo(self) -> str:
-        return f'{self.nome_do_distrito} {self.valor_do_distrito} {self.tipo_de_distrito.name}'
+        aux = f'{self.nome_do_distrito} ({self.valor_do_distrito} - {self.tipo_de_distrito.name})'
+        if self.efeito:
+            return aux
+        return f'{aux} \n{self.efeito}'
         
