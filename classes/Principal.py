bc53590fdd17d8c4086982ab19422b4eecd57e41
grandofelipe <felipegrando@uffs.edu.br>
2023-05-02 19:37:10 -0300

    Inicio da estratégia Felipe

--- classes/Principal.py
+++ classes/Principal.py
@@ -1,9 +1,10 @@
 from Simulacao import Simulacao
+from classes.strategies.EstrategiaFelipe import EstrategiaFelipe
 from classes.strategies.EstrategiaTotalmenteAleatoria import EstrategiaTotalmenteAleatoria
 from classes.strategies.EstrategiaManual import EstrategiaManual
 
 estrategias = (EstrategiaTotalmenteAleatoria(),
-               EstrategiaTotalmenteAleatoria(),
+               EstrategiaFelipe(),
                EstrategiaTotalmenteAleatoria(),
                EstrategiaTotalmenteAleatoria())
 simulacao = Simulacao(estrategias)
