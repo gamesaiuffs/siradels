1830e09459d1c3942546730cccb735b2448031f9
Djonatan Bonelli <100157600+FenrirTG1@users.noreply.github.com>
2023-07-16 18:25:24 -0300

    Correção da linha nula, remoções gerais

--- classes/ClassificaEstados.py
+++ classes/ClassificaEstados.py
@@ -10,7 +10,6 @@ from classes.strategies.EstrategiaJoao import EstrategiaJoao
 from classes.strategies.EstrategiaAndrei import EstrategiaAndrei
 from classes.strategies.EstrategiaMCTS import EstrategiaMCTS
 from classes.strategies.EstrategiaTotalmenteAleatoria import EstrategiaTotalmenteAleatoria
-from classes.enum.TipoTabelaEstado import TipoTabelaEstado
 from classes.enum.TipoDistrito import TipoDistrito
 
 class ClassificaEstados:    
@@ -18,26 +17,9 @@ class ClassificaEstados:
 
     @staticmethod
     def inicializar_estados(num_jogadores: int = 6, valor_inicial: int = 0) -> list[np.ndarray]:
-        '''
-        # 6 jogadores
-        modelo.append(np.ones((num_turnos, num_jogadores*2))*valor_inicial)  # Qtd ouro [0,1,2,3,4,5,>=6] = 112
-        modelo.append(np.ones((num_turnos, num_jogadores*2))*valor_inicial)  # Qtd carta mao [0,1,2,3,4,>=5] = 96
-        modelo.append(np.ones((num_turnos, num_jogadores*2))*valor_inicial)  # Carta mao mais cara [1 a 6] = 112
-        modelo.append(np.ones((num_turnos, num_jogadores*2))*valor_inicial)  # Carta mao mais barata [1 a 6] = 112
-        modelo.append(np.ones((num_turnos, num_jogadores*2))*valor_inicial)  # Qtd distritos construido [0 a 6] = 112
-        modelo.append(np.ones((num_turnos, num_jogadores*2))*valor_inicial)  # Qtd distrito construido Militar [0,1,2,>=3] = 64
-        modelo.append(np.ones((num_turnos, num_jogadores*2))*valor_inicial)  # Qtd distrito construido Religioso [0,1,2,>=3] = 64
-        modelo.append(np.ones((num_turnos, num_jogadores*2))*valor_inicial)  # Qtd distrito construido Nobre [0,1,2,>=3] = 64
-        modelo.append(np.ones((num_turnos, num_jogadores*2))*valor_inicial)  # Qtd distrito construido especiais [0,1,2,>=3] = 64
-        modelo.append(np.ones((num_turnos, num_jogadores*2))*valor_inicial)  # Qtd personagens disponiveis [2,3,4,5,6,7] = 128
-        modelo.append(np.ones((num_turnos, num_jogadores*2))*valor_inicial)  # Pontuacao [0-3,4-7,8-11,12-15,16-19,20-23,>=24] = 112 #
-        modelo.append(np.ones((num_turnos, num_jogadores*2))*valor_inicial)  # Personagem disponivel para escolha [1,2,3,4,5,6,7,8] = 8176
-        modelo.append(np.ones((num_turnos, num_jogadores*2))*valor_inicial)  # Personagem visivel descartado [1,2,3,5,6,7,8] = 3088
-        #modelo.append(np.ones((num_turnos, num_jogadores*2))*valor_inicial)  # Distancia do rei        
-        '''
+        
         """
-        MODELO:
-
+        MODELO (índices das linhas):
         QtdOuro = 0
         QtdCarta = 1
         CartaCara = 2
@@ -55,9 +37,6 @@ class ClassificaEstados:
         #DistanciaDoRei = 13
         """
 
-        # Array com estado do jogador
-        #modelo.append(np.ones(14)*valor_inicial)
-
         # Matriz com estado dos jogadores
         modelo = [(np.ones((num_jogadores, 14), dtype=np.ndarray)*valor_inicial)]
         
@@ -70,16 +49,6 @@ class ClassificaEstados:
             j = j.astype(np.uint32)
             np.savetxt('./classes/tabela_estado/' + 'jogo' + str(num_turnos) + '.csv', j, delimiter=',', fmt='%6u')
             #np.savetxt('./tabela/' + j.name + '.csv', i, delimiter=',', fmt='%6u')
-
-    # Carrega o modelo a partir dos arquivos CSV
-    @staticmethod
-    def ler_modelo() -> list[np.ndarray]:
-        modelo = []
-        for i in TipoTabelaEstado:
-            a = np.genfromtxt('./classes/tabela_estado/' + i.name + '.csv', delimiter=',')
-            #a = np.genfromtxt('./tabela/' + i.name + '.csv', delimiter=',')
-            modelo.append(a)
-        return modelo
 
     # Inicializa as tabelas e salva os estados parciais do número de partidas
     def simula_estados(self, qtd_partidas: int):
@@ -95,8 +64,19 @@ class ClassificaEstados:
                 # Inicializa variaveis para nova simulacao do jogo
                 historico = self.inicializar_estados(6)
                 estrategias = []
+                
                 for i in range(qtd_jogadores):          # fixo em 6 players
                     estrategias.append(EstrategiaTotalmenteAleatoria(str(i+1)))
+                
+                # Estrategias fixas e especificas
+                '''
+                estrategias.append(EstrategiaDjonatan())
+                estrategias.append(EstrategiaAndrei())
+                estrategias.append(EstrategiaBernardo())
+                estrategias.append(EstrategiaFelipe())
+                estrategias.append(EstrategiaGustavo())
+                estrategias.append(EstrategiaJoao())
+                '''
                 # Cria simulacao
                 simulacao = Simulacao(estrategias)
                 # Executa simulacao
@@ -113,10 +93,26 @@ class ClassificaEstados:
                     religioso = 0
                     militar = 0
                     especial = 0
+                    
+                    if jogador.vencedor:
+                        venceu = 1
+                    else:
+                        venceu = 0
+
+                    for distrito in jogador.distritos_construidos:
+                        if distrito.tipo_de_distrito == TipoDistrito.Nobre:
+                            nobre += 1
+                        if distrito.tipo_de_distrito == TipoDistrito.Religioso:
+                            religioso += 1
+                        if distrito.tipo_de_distrito == TipoDistrito.Militar:
+                            militar += 1
+                        if distrito.tipo_de_distrito == TipoDistrito.Especial:
+                            especial += 1
+                        num_dist_cons += 1
                     ##
                     
                     # Jogador atual
-
+                    print(jogador.nome)
                     if jogador.nome == "Bot - 1":
                     
                     # Set de variaveis
@@ -128,22 +124,6 @@ class ClassificaEstados:
                             # Custo da carta mais barata
                             if carta.valor_do_distrito < carta_barata:
                                 carta_barata = carta.valor_do_distrito
-
-                        if jogador.vencedor:
-                            venceu = 1
-                        else:
-                            venceu = 0
-
-                        for distrito in jogador.distritos_construidos:
-                            if distrito.tipo_de_distrito == TipoDistrito.Nobre:
-                                nobre += 1
-                            if distrito.tipo_de_distrito == TipoDistrito.Religioso:
-                                religioso += 1
-                            if distrito.tipo_de_distrito == TipoDistrito.Militar:
-                                militar += 1
-                            if distrito.tipo_de_distrito == TipoDistrito.Especial:
-                                especial += 1
-                            num_dist_cons += 1
 
                         # Coleta estados
                         modelo[0][0, 0] = jogador.ouro
@@ -160,25 +140,12 @@ class ClassificaEstados:
                         modelo[0][0, 11] = 0
                         modelo[0][0, 12] = 0
                         modelo[0][0, 13] = venceu
-                    
+
+                        # Resolvido: linha da tabela nula 
+                        i -= 1
+
                     # Outros jogadores
                     else:
-
-                        if jogador.vencedor:
-                            venceu = 1
-                        else:
-                            venceu = 0
-
-                        for distrito in jogador.distritos_construidos:
-                            if distrito.tipo_de_distrito == TipoDistrito.Nobre:
-                                nobre += 1
-                            if distrito.tipo_de_distrito == TipoDistrito.Religioso:
-                                religioso += 1
-                            if distrito.tipo_de_distrito == TipoDistrito.Militar:
-                                militar += 1
-                            if distrito.tipo_de_distrito == TipoDistrito.Especial:
-                                especial += 1
-                            num_dist_cons += 1
 
                         modelo[0][i, 0] = jogador.ouro
                         modelo[0][i, 1] = len(jogador.cartas_distrito_mao)
@@ -196,7 +163,8 @@ class ClassificaEstados:
                         modelo[0][i, 12] = 0
                         # Talvez tire
                         modelo[0][i, 13] = venceu
-            
+
+                    # Controla a entrada de jogadores (linhas) na tabela
                     if i < 5:
                         i += 1
 
