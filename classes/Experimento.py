16ed7365f217fde70250da394b856f4d79556013
grandofelipe <felipegrando@uffs.edu.br>
2023-09-08 11:35:28 -0300

    Método para testar a simulação
    Criado método para testar simulação com jogadores manuais ou com jogadores que usam estratégia totalmente aleatória.
    Início da adaptação da estratégia MCTS (ainda não foi finalizado os ajustes para rodar o MCTS nessa nossa versão).

--- classes/Experimento.py
+++ classes/Experimento.py
@@ -7,6 +7,7 @@ from classes.strategies.EstrategiaFelipe import EstrategiaFelipe
 from classes.strategies.EstrategiaGustavo import EstrategiaGustavo
 from classes.strategies.EstrategiaJoao import EstrategiaJoao
 from classes.strategies.EstrategiaAndrei import EstrategiaAndrei
+from classes.strategies.EstrategiaManual import EstrategiaManual
 from classes.strategies.EstrategiaMCTS import EstrategiaMCTS
 from classes.strategies.EstrategiaTotalmenteAleatoria import EstrategiaTotalmenteAleatoria
 from classes.enum.TipoTabelaPersonagem import TipoTabelaPersonagem
@@ -24,23 +25,8 @@ class Experimento:
     @staticmethod
     def inicializar_modelo_mcts(qtd_acoes: int, valor_inicial: int = 1) -> list[np.ndarray]:
         modelo = []
-        # Tamanho total 12 672 células para 8 ações
-        modelo.append(np.ones((7, qtd_acoes*2))*valor_inicial)  # Qtd ouro [0,1,2,3,4,5,>=6] = 112
-        modelo.append(np.ones((6, qtd_acoes*2))*valor_inicial)  # Qtd carta mão [0,1,2,3,4,>=5] = 96
-        modelo.append(np.ones((7, qtd_acoes*2))*valor_inicial)  # Carta mão mais cara [1 a 6] = 112
-        modelo.append(np.ones((7, qtd_acoes*2))*valor_inicial)  # Carta mão mais barata [1 a 6] = 112
-        modelo.append(np.ones((7, qtd_acoes*2))*valor_inicial)  # Qtd distritos construido [0 a 6] = 112
-        modelo.append(np.ones((4, qtd_acoes*2))*valor_inicial)  # Qtd distrito construido Militar [0,1,2,>=3] = 64
-        modelo.append(np.ones((4, qtd_acoes*2))*valor_inicial)  # Qtd distrito construido Religioso [0,1,2,>=3] = 64
-        modelo.append(np.ones((4, qtd_acoes*2))*valor_inicial)  # Qtd distrito construido Nobre [0,1,2,>=3] = 64
-        modelo.append(np.ones((8, qtd_acoes*2))*valor_inicial)  # Qtd personagens disponíveis [2,3,4,5,6,7] = 128
-        modelo.append(np.ones((7, qtd_acoes*2))*valor_inicial)  # Pontuacao [0-3,4-7,8-11,12-15,16-19,20-23,>=24] = 112
-        modelo.append(np.ones((7, qtd_acoes*2))*valor_inicial)  # Qtd distrito construido [0 a 6] = 112
-        modelo.append(np.ones((7, qtd_acoes*2))*valor_inicial)  # Qtd ouro [0,1,2,3,4,5,>=6] = 112
-        modelo.append(np.ones((6, qtd_acoes * 2)) * valor_inicial)  # Qtd carta mão [0,1,2,3,4,>=5] = 96
-        modelo.append(np.ones((511, qtd_acoes*2))*valor_inicial)  # Personagem disponivel para escolha [1,2,3,4,5,6,7,8] = 8176
-        modelo.append(np.ones((7, qtd_acoes * 2)) * valor_inicial)  # Quantidade de jogadores [4,5,6] = 112
-        modelo.append(np.ones((193, qtd_acoes*2))*valor_inicial)  # Personagem visivel descartado [1,2,3,5,6,7,8] = 3088
+        for tabela in TipoTabelaPersonagem:
+            modelo.append(np.ones((tabela.value, qtd_acoes*2)) * valor_inicial)
         return modelo
 
     # Salva o modelo em arquivos CSV
@@ -62,39 +48,39 @@ class Experimento:
         qtd_acoes = 8
         modelo = self.inicializar_modelo_mcts(qtd_acoes)
         qtd_simulacao = 0
+        # Fixado quantidade de jogadores em 5
+        qtd_jogadores = 5
         # while True:
         while tempo_limite > time.time() - inicio:
-            for qtd_jogadores in range(4, 7):
-                for modo in TipoTabelaPersonagem:
-                    # Faz um teste premiliminar a cada 300.000 (estimado em 1 arquivo por hora) jogos de treino e salva os resultados
-                    if qtd_simulacao % 1000 == 0:
-                        self.testar_modelo_gravar(5000, 6, qtd_simulacao)
+            for modo in TipoTabelaPersonagem:
+                # Faz um teste premiliminar a cada 300.000 (estimado em 1 arquivo por hora) jogos de treino e salva os resultados
+                if qtd_simulacao % 1000 == 0:
+                    self.testar_modelo_gravar(5000, 6, qtd_simulacao)
 
-                    # Inicializa variáveis para nova simulação do jogo
-                    qtd_simulacao += 1
-                    historico = self.inicializar_modelo_mcts(qtd_acoes, 0)
-                    estrategias = [EstrategiaMCTS(modelo, historico, modo)]
-                    for i in range(qtd_jogadores - 1):
-                        estrategias.append(EstrategiaTotalmenteAleatoria(str(i+1)))
-                    # Cria simulação
-                    simulacao = Simulacao(estrategias)
-                    # Executa simulação
-                    estado_final = simulacao.rodar_simulacao()
-                    # Atualizar modelo com vitórias e ações escolhidas
-                    for jogador in estado_final.jogadores:
-                        if jogador.nome == 'Bot - MCTS':
-                            if jogador.vencedor:
-                                for tabela, tabela_hist in zip(modelo, historico):
-                                    tabela += tabela_hist
-                            else:
-                                metade_tabela = int(modelo[0].shape[1] / 2)
-                                for tabela, tabela_hist in zip(modelo, historico):
-                                    tabela[:, metade_tabela:] += tabela_hist[:, metade_tabela:]
+                # Inicializa variáveis para nova simulação do jogo
+                qtd_simulacao += 1
+                historico = self.inicializar_modelo_mcts(qtd_acoes, 0)
+                estrategias = [EstrategiaMCTS(modelo, historico, modo)]
+                for i in range(qtd_jogadores - 1):
+                    estrategias.append(EstrategiaTotalmenteAleatoria(str(i+1)))
+                # Cria simulação
+                simulacao = Simulacao(estrategias)
+                # Executa simulação
+                estado_final = simulacao.rodar_simulacao()
+                # Atualizar modelo com vitórias e ações escolhidas
+                for jogador in estado_final.jogadores:
+                    if jogador.nome == 'Bot - MCTS':
+                        if jogador.vencedor:
+                            for tabela, tabela_hist in zip(modelo, historico):
+                                tabela += tabela_hist
+                        else:
+                            metade_tabela = int(modelo[0].shape[1] / 2)
+                            for tabela, tabela_hist in zip(modelo, historico):
+                                tabela[:, metade_tabela:] += tabela_hist[:, metade_tabela:]
             self.salvar_modelo(modelo)
 
-    # Terminar método
     # Aplica o modelo aprendido durante o número de simulações desejado para coletar o desempenho do modelo
-    def testar_modelo_mcts(self, qtd_simulacao_maximo: int, qtd_jogadores: int):
+    def testar_modelo_mcts(self, qtd_simulacao_maximo: int, qtd_jogadores: int = 5):
         modelo = self.ler_modelo()
         qtd_simulacao = 1
         resultados: dict[str, (int, int)] = dict()
@@ -104,6 +90,41 @@ class Experimento:
             estrategias.append(EstrategiaTotalmenteAleatoria(str(i + 1)))
         # Cria simulação
         simulacao = Simulacao(estrategias)
+        # Executa simulação
+        estado_final = simulacao.rodar_simulacao()
+        for jogador in estado_final.jogadores:
+            resultados[jogador.nome] = (int(jogador.vencedor), jogador.pontuacao_final)
+        while qtd_simulacao < qtd_simulacao_maximo:
+            qtd_simulacao += 1
+            # Cria simulação
+            simulacao = Simulacao(estrategias)
+            # Executa simulação
+            estado_final = simulacao.rodar_simulacao()
+            for jogador in estado_final.jogadores:
+                (vitoria, pontuacao) = resultados[jogador.nome]
+                resultados[jogador.nome] = (int(jogador.vencedor) + vitoria, jogador.pontuacao_final + pontuacao)
+        print()
+        for jogador, resultado in resultados.items():
+            (vitoria, pontuacao) = resultado
+            pontuacao_media = pontuacao / qtd_simulacao
+            print(
+                f'{jogador} - \tVitórias: {vitoria} - Porcento Vitorias: {vitoria / qtd_simulacao * 100:.2f}% - Pontuação Média: {pontuacao_media}')
+
+    # Testa a simulaçào do jogo apenas com a estratégia aleatória
+    @staticmethod
+    def testar_simulacao(manual: bool = False, qtd_simulacao_maximo: int = 1000, qtd_jogadores: int = 5):
+        qtd_simulacao = 1
+        resultados: dict[str, (int, int)] = dict()
+        # Inicializa variáveis para simulações do jogo
+        estrategias = []
+        if not manual:
+            for i in range(qtd_jogadores):
+                estrategias.append(EstrategiaTotalmenteAleatoria(str(i + 1)))
+            simulacao = Simulacao(estrategias)
+        else:
+            for i in range(qtd_jogadores):
+                estrategias.append(EstrategiaManual())
+            simulacao = Simulacao(estrategias, 8, False)
         # Executa simulação
         estado_final = simulacao.rodar_simulacao()
         for jogador in estado_final.jogadores:
