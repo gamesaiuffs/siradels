03ee3889ff2da8fb58ba25d20ad6b2dae0ca058d
Djonatan Bonelli <100157600+DjonatanBonelli@users.noreply.github.com>
2024-09-16 08:42:59 -0300

    Combinações Para Coleta Amostras

--- classes/classification/ColetaEstados.py
+++ classes/classification/ColetaEstados.py
@@ -2,9 +2,16 @@ from classes.classification.ClassificaEstados import ClassificaEstados
 from classes.Simulacao import Simulacao
 from classes.classification.SimulacaoColeta import SimulacaoColeta
 from classes.strategies.EstrategiaTotalmenteAleatoria import EstrategiaTotalmenteAleatoria
+from classes.strategies.EstrategiaAndrei import EstrategiaAndrei
+from classes.strategies.EstrategiaDjonatan import EstrategiaDjonatan
+from classes.strategies.EstrategiaFelipe import EstrategiaFelipe
+from classes.strategies.EstrategiaLuis import EstrategiaLuis
+from classes.strategies.Agente import Agente
 import numpy as np
 import random
+import itertools
 
+estrategias_disponiveis = [EstrategiaDjonatan(), EstrategiaAndrei(), EstrategiaFelipe(), EstrategiaLuis(), Agente()]
 
 class ColetaEstados:
     @staticmethod
@@ -12,39 +19,37 @@ class ColetaEstados:
         X_inicial = [np.zeros(n_features)]
         X = X_inicial
         Y = []
+        qtd_jogadores = 5
 
         qtd_simulacao = 0
+
+        combinacoes_estrategias = list(itertools.permutations(estrategias_disponiveis, qtd_jogadores))
+
         while qtd_simulacao < qtd_partidas:
-            for qtd_jogadores in range(5, 6):       ## ajustar quantidade de jogadores (original: range(4,7))
-                qtd_simulacao += 1
+
+            qtd_simulacao += 1
+            
+            # Inicializa variaveis para nova simulacao do jogo
+            estrategias = []
+            
+            combinacao_atual = combinacoes_estrategias[qtd_simulacao % len(combinacoes_estrategias)]
+        
+            for i in range(qtd_jogadores):
+                estrategia_classe = combinacao_atual[i]
+                estrategias.append(estrategia_classe(str(i+1)))
                 
-                # Inicializa variaveis para nova simulacao do jogo
-                estrategias = []
-                
-                for i in range(qtd_jogadores):          # fixo em 5 players
-                    estrategias.append(EstrategiaTotalmenteAleatoria(str(i+1)))
-                
-                # Estrategias fixas e especificas
-                '''
-                estrategias.append(EstrategiaDjonatan())
-                estrategias.append(EstrategiaAndrei())
-                estrategias.append(EstrategiaBernardo())
-                estrategias.append(EstrategiaFelipe())
-                estrategias.append(EstrategiaGustavo())
-                estrategias.append(EstrategiaJoao())
-                '''
-                # Cria simulacao
-                simulacao = SimulacaoColeta(estrategias)
-                # Executa simulacao
-                X_coleta, Y_coleta, n_rodada = simulacao.rodar_simulacao(X_inicial, nome_modelo)
-                #print(qtd_partidas-qtd_simulacao)
-                # Remove primeira linha nula
-                X_coleta = np.delete(X_coleta, 0, axis=0)
-                # Empilha linhas na matriz
-                X = np.vstack((X, X_coleta))
-                # Atrubui rótulos de acordo com a quantidade de rodadas
-                for i in range(n_rodada):
-                    Y.append(Y_coleta)
+            # Cria simulacao
+            simulacao = SimulacaoColeta(estrategias)
+            # Executa simulacao
+            X_coleta, Y_coleta, n_rodada = simulacao.rodar_simulacao(X_inicial, nome_modelo)
+            #print(qtd_partidas-qtd_simulacao)
+            # Remove primeira linha nula
+            X_coleta = np.delete(X_coleta, 0, axis=0)
+            # Empilha linhas na matriz
+            X = np.vstack((X, X_coleta))
+            # Atrubui rótulos de acordo com a quantidade de rodadas
+            for i in range(n_rodada):
+                Y.append(Y_coleta)
         # Remove primeira linha nula
         X = np.delete(X, 0, axis=0)
         ClassificaEstados.salvar_amostras(X, Y, jogos, rotulos)
