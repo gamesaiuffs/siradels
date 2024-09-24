b948b70d9afa2859f36de438ffd3a133fdacc2b5
JeanFenner <jeancarlo.fenner@gmail.com>
2024-09-24 20:05:06 -0300

    Ultima versao EstrategiaJean

--- classes/strategies/EstrategiaJean.py
+++ classes/strategies/EstrategiaJean.py
@@ -10,8 +10,8 @@ import random
 
 
 class EstrategiaJean(Estrategia):
-    def __init__(self):
-        super().__init__('Jean.')
+    def __init__(self, nome: str= 'Jean'):
+        super().__init__(nome)
 
     # Estratégia usada na fase de escolha dos personagens
     @staticmethod
@@ -85,6 +85,10 @@ class EstrategiaJean(Estrategia):
     # Estratégia usada na fase de escolha das ações no turno
     @staticmethod
     def escolher_acao(estado: Estado, acoes_disponiveis: list[TipoAcao]) -> int:
+        # Coletar ouro
+        if TipoAcao.ColetarOuro in acoes_disponiveis:
+            return acoes_disponiveis.index(TipoAcao.ColetarOuro)
+        # Preferencia para habilidad de coleta se tiver menos de 3 ouros
         if estado.jogador_atual.ouro < 3:
             if TipoAcao.HabilidadeRei in acoes_disponiveis:
                 return acoes_disponiveis.index(TipoAcao.HabilidadeRei)
@@ -95,14 +99,18 @@ class EstrategiaJean(Estrategia):
             if TipoAcao.HabilidadeComerciante in acoes_disponiveis:
                 return acoes_disponiveis.index(TipoAcao.HabilidadeComerciante)
             if TipoAcao.HabilidadeSenhorDaGuerraColetar in acoes_disponiveis:
-                return acoes_disponiveis.index(TipoAcao.HabilidadeSenhorDaGuerraColetar)                                    
-        else:
-            if estado.jogador_atual.ouro > 4 and TipoAcao.ColetarCartas in acoes_disponiveis:
-                return acoes_disponiveis.index(TipoAcao.ColetarCartas)
-        # Deixa passar turno por último
+                return acoes_disponiveis.index(TipoAcao.HabilidadeSenhorDaGuerraColetar)
+        # Trocar cartas com a Ilusionista se a media de custo for menor que 3
         if TipoAcao.HabilidadeIlusionistaDescartar in acoes_disponiveis and len(estado.jogador_atual.cartas_distrito_mao)>0:
-            return acoes_disponiveis.index(TipoAcao.HabilidadeIlusionistaDescartar)
+            med = cnt = 0
+            for cartas in estado.jogador_atual.cartas_distrito_mao:
+                med += cartas.valor_do_distrito
+                cnt += 1
+            med = med/cnt
+            if med < 3:
+                return acoes_disponiveis.index(TipoAcao.HabilidadeIlusionistaDescartar)
 
+        # Deixa passar turno por último
         acao_escolhida = random.randint(0, len(acoes_disponiveis) - 1)
         while len(acoes_disponiveis) > 1 and acoes_disponiveis[acao_escolhida] == TipoAcao.PassarTurno:
             acao_escolhida = random.randint(0, len(acoes_disponiveis) - 1)
@@ -142,8 +150,6 @@ class EstrategiaJean(Estrategia):
         for personagem in opcoes_personagem:
             if personagem not in estado.tabuleiro.cartas_visiveis:
                 opcoes.append(personagem)
-        if TipoPersonagem.Comerciante in opcoes:
-            return opcoes.index(TipoPersonagem.Comerciante)
         return random.randint(0, len(opcoes) - 1)
 
     # Estratégia usada na habilidade do Ladrão
@@ -154,22 +160,30 @@ class EstrategiaJean(Estrategia):
         for personagem in opcoes_personagem:
             if personagem not in estado.tabuleiro.cartas_visiveis:
                 opcoes.append(personagem)
+        # Prioridade para a Arquiteta e o Rei
+        if TipoPersonagem.Arquiteta in opcoes:
+            return opcoes.index(TipoPersonagem.Arquiteta)
+        elif TipoPersonagem.Rei in opcoes:
+            return opcoes.index(TipoPersonagem.Rei) # <- nao sei pq ajudou mas...
         return random.randint(0, len(opcoes) - 1)
 
     # Estratégia usada na habilidade da Ilusionista (escolha do jogador alvo)
     @staticmethod
     def habilidade_ilusionista_trocar(estado: Estado, opcoes_jogadores: list[Jogador]) -> int:
-        return random.randint(0, len(opcoes_jogadores) - 1)
-        # Ilusionista sempre troca de mão com o adversário que possui mais cartas, o desempate é uma escolha aleatória entre empatados
-        mais_cartas = 0
+        # return random.randint(0, len(opcoes_jogadores) - 1)
+        # Ilusionista sempre troca de mão com o adversário que possui maior pontuacao
+        maior_pontuacao = 0
         for jogador in opcoes_jogadores:
-            if len(jogador.cartas_distrito_mao) > mais_cartas:
-                mais_cartas = len(jogador.cartas_distrito_mao)
+            if jogador.pontuacao > maior_pontuacao and len(jogador.cartas_distrito_mao) > 1:
+                maior_pontuacao = jogador.pontuacao
         opcoes = []
         for idx, jogador in enumerate(opcoes_jogadores):
-            if len(jogador.cartas_distrito_mao) == mais_cartas:
+            if jogador.pontuacao == maior_pontuacao:
                 opcoes.append(idx)
-        return random.sample(opcoes, 1)[0]
+        if len(opcoes)>1:
+            return random.sample(opcoes, 1)[0]
+        
+        return random.randint(0, len(opcoes_jogadores) - 1)
 
     # Estratégia usada na habilidade da Ilusionista (escolha de quantas cartas serão descartadas)
     @staticmethod
@@ -187,12 +201,24 @@ class EstrategiaJean(Estrategia):
     # Estratégia usada na habilidade da Ilusionista (escolha de qual carta descartar)
     @staticmethod
     def habilidade_ilusionista_descartar_carta(estado: Estado, qtd_cartas: int, i: int) -> int:
+        # Descarta cartas com valor menor que 2 ou maior que 5
+        for idx, carta in enumerate(estado.jogador_atual.cartas_distrito_mao):
+            if carta.valor_do_distrito < 2 or carta.valor_do_distrito > 5:
+                return idx
         return random.randint(0, len(estado.jogador_atual.cartas_distrito_mao) - 1)
 
     # Estratégia usada na habilidade do Senhor da Guerra
     @staticmethod
     def habilidade_senhor_da_guerra_destruir(estado: Estado, distritos_para_destruir: list[(CartaDistrito, Jogador)]) -> int:
-        return random.randint(0, len(distritos_para_destruir) - 1)
+        # Destroi distritos com valor menor que 2 apenas
+        opcoes = []
+        for idx, distrito in enumerate(distritos_para_destruir):
+            if distrito[0].valor_do_distrito < 2:
+                opcoes.append(idx)
+        if len(opcoes) > 0:
+            return random.randint(0, len(opcoes) - 1)
+        
+        return 0
 
     # Estratégia usada na ação do Laboratório
     @staticmethod
