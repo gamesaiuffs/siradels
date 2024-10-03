2402f9d6a3baa7425e96a47817233d9f52bbc693
Djonatan Bonelli <100157600+DjonatanBonelli@users.noreply.github.com>
2024-10-03 10:48:15 -0300

    Mostra estatísticas de coleta
    Rever pontuação média (33??)

--- classes/classification/ColetaEstados.py
+++ classes/classification/ColetaEstados.py
@@ -17,75 +17,88 @@ from classes.Experimento import Experimento
 import numpy as np
 import random
 from itertools import combinations, combinations_with_replacement
-import itertools
+from math import comb
 
 estrategias: list[Estrategia] = [EstrategiaAndrei(), EstrategiaDjonatan(), EstrategiaEduardo(),
                                  EstrategiaFelipe(), EstrategiaJean(), EstrategiaLuisII(),
                                  EstrategiaTotalmenteAleatoria()] #MCTS e Agente off, levar para ColetaEstados e adaptar
 # estrategias: list[Estrategia] = [Agente(imprimir=True), EstrategiaTotalmenteAleatoria("B2"), EstrategiaTotalmenteAleatoria("B3"), EstrategiaTotalmenteAleatoria("B4"), EstrategiaTotalmenteAleatoria("B5")]
-comb = list(combinations_with_replacement(estrategias, 5))
+combinacoes = list(combinations_with_replacement(estrategias, 5))
 
-qtd_comb = len(comb)
+qtd_comb = len(combinacoes)
 
-#print("Quantidade de Combinações:", qtd_comb)
+print("Quantidade de Combinações:", qtd_comb)
 
 class ColetaEstados:
     @staticmethod
-    def coleta_amostras(n_features: int, jogos: str, rotulos: str, nome_modelo: str = '', qtd_simulacao: int = 100,):
+    def coleta_amostras(n_features: int, jogos: str, rotulos: str, nome_modelo: str = '', qtd_simulacao: int = 100):
         X_inicial = [np.zeros(n_features)]
         X = X_inicial
         Y = []
         num_simulacao = 0
-        resultados: dict[str, (int, int, int, int, int, int)] = dict()
+
+        # Inicializa dicionários de resultados
         resultados_total: dict[str, (int, int, int, int, int, int, int)] = dict()
-        
-        while num_simulacao <= qtd_simulacao-1:
-            print(f"{num_simulacao+1}/{qtd_simulacao}")
+        resultados: dict[str, (int, int, int, int, int, int)] = dict()
 
-            for e in estrategias:
-                resultados_total[e.nome] = (0, 0, 0, 0, 0, 0, 0)
-            for i, p in enumerate(comb):
-                #if (i+1) % 100 == 0 or i+1 == qtd_comb:
-                #    print(f"{i+1}/{qtd_comb} - {((i+1)*100/qtd_comb):.2f}%")
+        # Inicializa resultados para cada estratégia
+        for i, jogador in enumerate(estrategias):
+            chave = f"{jogador.nome}"
+            resultados[chave] = (0, 0, 0, 0, 0, 0)
+            resultados_total[chave] = (0, 0, 0, 0, 0, 0, 0)  # Para o total
 
-            #resultados = Experimento.testar_estrategias(list(p), qtd_simulacao)
-                # Cria simulacao
+        while num_simulacao < qtd_simulacao:
+            print(f"Simulação {num_simulacao+1}/{qtd_simulacao}")
+
+            for i, p in enumerate(combinacoes):
+                # Executa simulação com a combinação de estratégias
                 simulacao = SimulacaoColeta(list(p))
-                # Executa simulacao
                 estado_final, X_coleta, Y_coleta, n_rodada = simulacao.rodar_simulacao(X_inicial, nome_modelo)
-                # Remove primeira linha nula
+                # Remove a primeira linha nula e empilha as amostras
                 X_coleta = np.delete(X_coleta, 0, axis=0)
-                # Empilha linhas na matriz
                 X = np.vstack((X, X_coleta))
-                # Atrubui rótulos de acordo com a quantidade de rodadas
-                for i in range(n_rodada):
-                    Y.append(Y_coleta)
-                '''
+
+                # Armazena rótulos
+                Y.extend(Y_coleta for _ in range(n_rodada))
+
+                # Atualiza os resultados da simulação
                 for jogador in estado_final.jogadores:
-                    (vitoria, seg, ter, qua, qui, pontuacao) = resultados[jogador.nome]
+                    chave = f"{jogador.nome}"
+                    (vitoria, seg, ter, qua, qui, pontuacao) = resultados[chave]
+                    
+                    # Atualiza as posições e pontuações de acordo com o estado final da simulação
                     if jogador == estado_final.jogadores[1]:
                         seg += 1
-                    if jogador == estado_final.jogadores[2]:
+                    elif jogador == estado_final.jogadores[2]:
                         ter += 1
-                    if jogador == estado_final.jogadores[3]:
+                    elif jogador == estado_final.jogadores[3]:
                         qua += 1
-                    if jogador == estado_final.jogadores[4]:
+                    elif jogador == estado_final.jogadores[4]:
                         qui += 1
-                    resultados[jogador.nome] = (int(jogador.vencedor) + vitoria, seg, ter, qua, qui, jogador.pontuacao_final + pontuacao)
-                '''
+
+                    resultados[chave] = (int(jogador.vencedor) + vitoria, seg, ter, qua, qui, jogador.pontuacao_final + pontuacao)
+            
+            # Conta quantas vezes uma estratégia apareceu em uma lista para a combinação
+            count = 0
             num_simulacao += 1
-        '''
-        for jogador, resultado in resultados.items():
-            (vitoria, seg, ter, qua, qui, pontuacao) = resultado
-            vitoria += resultados_total[jogador][0]
-            seg += resultados_total[jogador][1]
-            ter += resultados_total[jogador][2]
-            qua += resultados_total[jogador][3]
-            qui += resultados_total[jogador][4]
-            pontuacao += resultados_total[jogador][5]
-            resultados_total[jogador] = (vitoria, seg, ter, qua, qui, pontuacao, resultados_total[jogador][6] + qtd_simulacao)
-        '''
-        '''    
+            for i in combinacoes:
+                for j in i:
+                    if j.nome == estrategias[0].nome:
+                        count += 1
+                        break
+
+            # Acumula os resultados de todas as simulações
+            for jogador, resultado in resultados.items():
+                (vitoria, seg, ter, qua, qui, pontuacao) = resultado
+                vitoria += resultados_total[jogador][0]
+                seg += resultados_total[jogador][1]
+                ter += resultados_total[jogador][2]
+                qua += resultados_total[jogador][3]
+                qui += resultados_total[jogador][4]
+                pontuacao += resultados_total[jogador][5]
+                resultados_total[jogador] = (vitoria, seg, ter, qua, qui, pontuacao, resultados_total[jogador][6] + count)
+
+        # Calcula estatísticas finais para cada jogador
         for jogador, resultado in resultados_total.items():
             (vitoria, seg, ter, qua, qui, pontuacao, qtd_simulacao_total) = resultado
             pontuacao_media = pontuacao / qtd_simulacao_total
@@ -94,13 +107,14 @@ class ColetaEstados:
             taxa_ter = 100 * ter / qtd_simulacao_total
             taxa_qua = 100 * qua / qtd_simulacao_total
             taxa_qui = 100 * qui / qtd_simulacao_total
-        print(
-        f'\n{jogador} - Vitórias: {vitoria} - Taxa de Vitórias: {taxa_vitoria:.2f}% - Pontuação Média: {pontuacao_media:.2f}\n\t'
-        f'Primeiro: {taxa_vitoria:5.2f}%\n\tSegundo : {taxa_seg:5.2f}%\n\tTerceiro: {taxa_ter:5.2f}%\n\tQuarto  : {taxa_qua:5.2f}%\n\tQuinto  : {taxa_qui:5.2f}%')
+            
+            print(
+                f'\n{jogador} - Vitórias: {vitoria} - Taxa de Vitórias: {taxa_vitoria:.2f}% - Pontuação Média: {pontuacao_media:.2f}\n\t'
+                f'Primeiro: {taxa_vitoria:5.2f}%\n\tSegundo : {taxa_seg:5.2f}%\n\tTerceiro: {taxa_ter:5.2f}%\n\tQuarto  : {taxa_qua:5.2f}%\n\tQuinto  : {taxa_qui:5.2f}%'
+            )
         print("Fim dos testes das estratégias")
-        '''
            
         # Remove primeira linha nula
         X = np.delete(X, 0, axis=0)
-        ClassificaEstados.salvar_amostras(X, Y, jogos, rotulos)
+        #ClassificaEstados.salvar_amostras(X, Y, jogos, rotulos)
         #ClassificaEstados.treinar_modelo(X, Y)
